3997e1b7d8c73ac7e5cfae551e291bd1ee327eed
void1007 <145522686+void1007@users.noreply.github.com>
2024-04-16 17:28:25 +0900

    Update HelloController.java

--- spring/firstapp/src/main/java/dw/firstapp/controller/HelloController.java
+++ spring/firstapp/src/main/java/dw/firstapp/controller/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/sayhello")
+    @GetMapping("/Hellogays")
     public String hello(){
-        return "Hello World!";
+        return "Hello gays!!";
     }
 }
 
